c0b6871d25243996f0fdbd682ee506b87be90408
SeongGwangJu <seonggwangdev@gmail.com>
2024-08-27 16:38:41 +0900

    first commit

---
+++ src/main/java/com/chicchoc/sivillage/SiVillageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chicchoc.sivillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiVillageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiVillageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chicchoc/sivillage/SiVillageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chicchoc.sivillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiVillageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
