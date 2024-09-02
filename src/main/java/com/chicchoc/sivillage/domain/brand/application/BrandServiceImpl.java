9de6db7fd8f7ce47245ed65df76986b331724e6c
jaehunLee-git <joz526@naver.com>
2024-09-02 21:11:30 +0900

    Feat : 브랜드 조회 기능 추가

---
+++ src/main/java/com/chicchoc/sivillage/domain/brand/application/BrandServiceImpl.java
@@ -0,0 +1,31 @@
+package com.chicchoc.sivillage.domain.brand.application;
+
+import com.chicchoc.sivillage.domain.brand.domain.Brand;
+import com.chicchoc.sivillage.domain.brand.dto.out.BrandResponseDto;
+import com.chicchoc.sivillage.domain.brand.infrastructure.BrandRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class BrandServiceImpl implements BrandService {
+
+    private final BrandRepository brandRepository;
+
+    @Override
+    public List<BrandResponseDto> getBrands() {
+        List<Brand> brands = brandRepository.findAll();
+
+        return brands.stream()
+                .map(brand -> BrandResponseDto.builder()
+                        .id(brand.getId())
+                        .name(brand.getName())
+                        .logoUrl(brand.getLogoUrl())
+                        .build())
+                .toList();
+    }
+}
