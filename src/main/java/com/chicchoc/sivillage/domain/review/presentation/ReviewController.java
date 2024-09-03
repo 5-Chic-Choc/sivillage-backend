42681383309b5cd09d38ec1d7f96c00f39f2fa9d
flskhhdf <flskhhdf@naver.com>
2024-09-03 20:08:17 +0900

    Feat : 리뷰 컨트롤러 수정 - createReview의 리턴값을 CommonResponseEntity로 수정

--- src/main/java/com/chicchoc/sivillage/domain/review/presentation/ReviewController.java
+++ src/main/java/com/chicchoc/sivillage/domain/review/presentation/ReviewController.java
@@ -3,7 +3,9 @@ package com.chicchoc.sivillage.domain.review.presentation;
 import com.chicchoc.sivillage.domain.review.application.ReviewService;
 import com.chicchoc.sivillage.domain.review.dto.in.ReviewRequestDto;
 import com.chicchoc.sivillage.domain.review.vo.in.ReviewRequestVo;
+import com.chicchoc.sivillage.global.common.entity.CommonResponseEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,7 @@ public class ReviewController {
   private final ReviewService reviewService;
 
   @PostMapping
-  public void createReview(@RequestBody ReviewRequestVo reviewRequestVo) {
+  public CommonResponseEntity<Void> createReview(@RequestBody ReviewRequestVo reviewRequestVo) {
     ReviewRequestDto reviewRequestDto = ReviewRequestDto.builder()
         .productId(reviewRequestVo.getProductId())
         .size(reviewRequestVo.getSize())
@@ -26,7 +28,14 @@ public class ReviewController {
         .rate(reviewRequestVo.getRate())
         .content(reviewRequestVo.getContent())
         .build();
-    // TODO CommonResponseEntity 필요
     reviewService.addReview(reviewRequestDto);
+    return new CommonResponseEntity<>(
+        HttpStatus.CREATED,
+        "리뷰 작성 성공",
+        null
+    );
   }
 }
+
+
+
