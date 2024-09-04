dec6167a401690c843b0174bde8069848f6560f7
ddu2 <flskhhdf@naver.com>
2024-09-04 09:43:43 +0900

    Docs : config 파일 생성 - checkstyle 파일 생성 및 google_checks.yml 파일 추가 - style 파일 생성

---
+++ src/main/java/com/chicchoc/sivillage/domain/review/vo/out/ReviewResponseVo.java
@@ -0,0 +1,13 @@
+package com.chicchoc.sivillage.domain.review.vo.out;
+
+import java.util.Date;
+
+public class ReviewResponseVo {
+    private Long id;
+    private Long productId;
+    private String size;
+    private String info;
+    private int rate;
+    private String content;
+    private Date createAt;
+}
