2647c70916cbbe75ec7f0f9ecb97908bc8d11330
SeongGwang <seonggwangdev@gmail.com>
2024-09-04 17:11:14 +0900

    Refactor : JWT Property
    - 기존 env 변수를 직접 입력 -> Getter 형식으로 받아오도록 수정하여 보다 안정적이고 유지보수를 용이하게 함

---
+++ src/main/java/com/chicchoc/sivillage/global/auth/jwt/JwtProperties.java
@@ -0,0 +1,22 @@
+package com.chicchoc.sivillage.global.auth.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    //자바 클래스에 jwt 프로퍼티값을 가져와 사용할 수 있게 함
+
+    private String issuer;
+    private String secretKey;
+    private long accessExpireTime;
+    private long refreshExpireTime;
+    private String tokenPrefix;
+    private String headerString;
+
+}
