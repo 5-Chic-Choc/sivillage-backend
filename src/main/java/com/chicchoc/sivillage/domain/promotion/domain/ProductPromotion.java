d330a1cb4694c177ba0fab250262aedf81ab3a40
jaehunLee-git <joz526@naver.com>
2024-09-04 10:02:55 +0900

    Feat : add promotion Entity class

--- src/main/java/com/chicchoc/sivillage/domain/promotion/domain/ProductPromotion.java
+++ src/main/java/com/chicchoc/sivillage/domain/promotion/domain/ProductPromotion.java
@@ -1,4 +1,27 @@
 package com.chicchoc.sivillage.domain.promotion.domain;
 
+import com.chicchoc.sivillage.domain.product.domain.Product;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
 public class ProductPromotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "promotion_id")
+    private Promotion promotion;
+
+    @Comment("프로모션 타입")
+    @Column(nullable = false, length = 50)
+    private String promotionType;
 }
--- src/main/java/com/chicchoc/sivillage/domain/promotion/domain/Promotion.java
+++ src/main/java/com/chicchoc/sivillage/domain/promotion/domain/Promotion.java
@@ -1,4 +1,32 @@
 package com.chicchoc.sivillage.domain.promotion.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
 public class Promotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "promotion_id")
+    private Long id;
+
+    @Comment("제목")
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Comment("프로모션 간단 설명")
+    @Column(nullable = false, length = 255)
+    private String description;
+
+    @Comment("프로모션 상세 내용")
+    @Column(nullable = false)
+    @Lob
+    private String content;
+
+    @Comment("썸네일 이미지 URL")
+    @Column(nullable = false, length = 2000)
+    private String thumbnailUrl;
 }
