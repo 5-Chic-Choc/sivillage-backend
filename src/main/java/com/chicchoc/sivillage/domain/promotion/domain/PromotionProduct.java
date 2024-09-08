4da6a382e211e12f354527c60d510ac238fae570
jaehunLee-git <joz526@naver.com>
2024-09-08 16:27:24 +0900

    Refactor : 프로모션 관련 Entity 수정

--- src/main/java/com/chicchoc/sivillage/domain/promotion/domain/PromotionProduct.java
+++ src/main/java/com/chicchoc/sivillage/domain/promotion/domain/PromotionProduct.java
@@ -1,25 +1,25 @@
 package com.chicchoc.sivillage.domain.promotion.domain;
 
-import com.chicchoc.sivillage.domain.product.domain.Product;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.hibernate.annotations.Comment;
 
 @Entity
 @Getter
-public class ProductPromotion {
+public class PromotionProduct {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "promotion_product_id")
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
-    private Product product;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "promotion_id")
     private Promotion promotion;
+
+    @Comment("상품 UUID")
+    @Column(nullable = false, length = 21)
+    private String productUuid;
 
     @Comment("프로모션 타입")
     @Column(nullable = false, length = 50)
