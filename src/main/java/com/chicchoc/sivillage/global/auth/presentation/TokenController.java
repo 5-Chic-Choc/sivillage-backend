cc1edae3c8b1efce14a8adb271151e4ff763ee1f
SeongGwang <seonggwangdev@gmail.com>
2024-09-08 17:15:17 +0900

    Refactor : Access token 재발급 위치 수정
    - 기존 String에서 헤더 발급으로 수정

--- src/main/java/com/chicchoc/sivillage/global/auth/presentation/TokenController.java
+++ src/main/java/com/chicchoc/sivillage/global/auth/presentation/TokenController.java
@@ -3,7 +3,9 @@ package com.chicchoc.sivillage.global.auth.presentation;
 import com.chicchoc.sivillage.global.auth.application.TokenService;
 import com.chicchoc.sivillage.global.auth.dto.CreateAccessTokenRequestDto;
 import com.chicchoc.sivillage.global.auth.dto.CreateAccessTokenResponseDto;
+import com.chicchoc.sivillage.global.auth.jwt.JwtProperties;
 import com.chicchoc.sivillage.global.common.entity.CommonResponseEntity;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,17 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class TokenController {
 
     private final TokenService tokenService;
+    private final JwtProperties jwtProperties;
 
     @PostMapping("/refresh")
     public CommonResponseEntity<CreateAccessTokenResponseDto> createNewAccessToken(
-            @RequestBody CreateAccessTokenRequestDto requestDto) {
+            @RequestBody CreateAccessTokenRequestDto requestDto, HttpServletResponse response) {
 
         String newAccessToken = tokenService.createNewAccessToken(requestDto.getRefreshToken());
+        response.setHeader(jwtProperties.getHeaderString(), newAccessToken);
 
         return new CommonResponseEntity<>(
                 HttpStatus.CREATED,
                 "새로운 액세스 토큰이 발급되었습니다.",
-                new CreateAccessTokenResponseDto(newAccessToken)
+                null
         );
     }
 }
