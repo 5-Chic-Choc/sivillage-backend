687a90030459a3f70647d3349426cd2947193070
ddu2 <flskhhdf@naver.com>
2024-09-08 19:00:00 +0900

    Feat : 장바구니 제품 controller 분리

--- src/main/java/com/chicchoc/sivillage/domain/cart/presentation/CartProductController.java
+++ src/main/java/com/chicchoc/sivillage/domain/cart/presentation/CartProductController.java
@@ -1,14 +1,9 @@
 package com.chicchoc.sivillage.domain.cart.presentation;
 
 import com.chicchoc.sivillage.domain.cart.application.CartProductService;
-import com.chicchoc.sivillage.domain.cart.application.CartService;
 import com.chicchoc.sivillage.domain.cart.dto.out.CartProductResponseDto;
-import com.chicchoc.sivillage.domain.cart.dto.out.CartResponseDto;
 import com.chicchoc.sivillage.domain.cart.vo.out.CartProductResponseVo;
-import com.chicchoc.sivillage.domain.cart.vo.out.CartResponseVo;
 import com.chicchoc.sivillage.global.common.entity.CommonResponseEntity;
-import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,27 +11,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/cart")
-public class CartController {
+@RequestMapping("/api/v1/cartProduct")
+public class CartProductController {
 
-    private final CartService cartService;
     private final CartProductService cartProductService;
 
-    @GetMapping("/list")
-    public CommonResponseEntity<List<CartResponseVo>> getCartList(){
-        // jwt로 userUuid 가져오기
-        String userUuid = "1";
-        List<CartResponseDto> cartResponseDto = cartService.getCartUuidList(userUuid);
-        List<CartResponseVo> cartResponseVoList = cartResponseDto.stream()
-                .map(CartResponseDto::toCartResponseVo)
+    @GetMapping("/list/{cartUuid}")
+    public CommonResponseEntity<List<CartProductResponseVo>> getProductList(@PathVariable("cartUuid") String cartUuid){
+        List<CartProductResponseDto> cartProductResponseDto = cartProductService.getCartProductList(cartUuid);
+        List<CartProductResponseVo> cartProductResponseVo = cartProductResponseDto.stream()
+                .map(CartProductResponseDto::toCartProductResponseVo)
                 .toList();
-
         return new CommonResponseEntity<>(
                 HttpStatus.OK,
-                "장바구니 불러오기 성공",
-                cartResponseVoList
+                "장바구니 제품 불러오기 성공",
+                cartProductResponseVo
         );
     }
 }
