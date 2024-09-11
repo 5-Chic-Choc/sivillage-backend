245e204e40adfea780496ef623cde08122e4f5f7
jaehunLee-git <joz526@naver.com>
2024-09-11 18:36:34 +0900

    Refactor : Vo, Dto 수정

--- src/main/java/com/chicchoc/sivillage/domain/product/dto/out/ProductResponseDto.java
+++ src/main/java/com/chicchoc/sivillage/domain/product/dto/out/ProductResponseDto.java
@@ -5,28 +5,26 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
 public class ProductResponseDto {
     private String productUuid;
-    private String productName;
-    private int price;
-    private int discountRate;
-    private int discountPrice;
+    private String brandUuid;
+    private String name;
     private LocalDateTime createdAt;
-    private Long brandId;
+    private LocalDateTime updatedAt;
+    private List<ProductOptionResponseDto> productOptions;
 
     // VO로 변환하는 메소드
     public ProductResponseVo toResponseVo() {
         return ProductResponseVo.builder()
                 .productUuid(productUuid)
-                .productName(productName)
-                .price(price)
-                .discountRate(discountRate)
-                .discountPrice(discountPrice)
+                .brandUuid(brandUuid)
+                .name(name)
                 .createdAt(createdAt)
-                .brandId(brandId)
+                .updatedAt(updatedAt)
                 .build();
     }
 }
