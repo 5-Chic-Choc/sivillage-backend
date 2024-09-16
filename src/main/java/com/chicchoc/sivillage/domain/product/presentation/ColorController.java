f960458d4367c09a21e264e5d14b3090f8290583
jaehunLee-git <joz526@naver.com>
2024-09-16 16:50:43 +0900

    Feat : Color 엔티티 관련 전체 조회 및 단일 조회 기능 추가

---
+++ src/main/java/com/chicchoc/sivillage/domain/product/presentation/ColorController.java
@@ -0,0 +1,39 @@
+package com.chicchoc.sivillage.domain.product.presentation;
+
+import com.chicchoc.sivillage.domain.product.application.ColorService;
+import com.chicchoc.sivillage.domain.product.dto.out.ColorResponseDto;
+import com.chicchoc.sivillage.domain.product.vo.out.ColorResponseVo;
+import com.chicchoc.sivillage.global.common.entity.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/colors")
+public class ColorController {
+
+    private final ColorService colorService;
+
+    @Operation(summary = "getAllColors API", description = "모든 색상 조회", tags = {"Color"})
+    @GetMapping
+    public BaseResponse<List<ColorResponseVo>> getAllColors() {
+        List<ColorResponseDto> colorDtos = colorService.getAllColors();
+        List<ColorResponseVo> colorVos = colorDtos.stream()
+                .map(ColorResponseDto::toVo)
+                .toList();
+        return new BaseResponse<>(colorVos);
+    }
+
+    @Operation(summary = "getColorById API", description = "색상 상세 조회", tags = {"Color"})
+    @GetMapping("/{id}")
+    public BaseResponse<ColorResponseVo> getColorById(@PathVariable Long id) {
+        ColorResponseDto colorDto = colorService.getColorById(id);
+        return new BaseResponse<>(colorDto.toVo());
+    }
+}
