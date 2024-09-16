ec2f57cce4a05b0cf150d08c830719dbd50d8d42
jaehunLee-git <joz526@naver.com>
2024-09-16 16:50:07 +0900

    Feat : Size 엔티티 관련 전체 조회 및 단일 조회 기능 추가

---
+++ src/main/java/com/chicchoc/sivillage/domain/product/vo/out/SizeResponseVo.java
@@ -0,0 +1,11 @@
+package com.chicchoc.sivillage.domain.product.vo.out;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SizeResponseVo {
+    private Long id;
+    private String name;
+}
