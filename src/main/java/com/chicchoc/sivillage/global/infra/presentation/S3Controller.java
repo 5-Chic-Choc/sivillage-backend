713ebb279f53a61aa92f4cf66067d815b1b53384
flskhhdf <flskhhdf@naver.com>
2024-09-18 15:21:58 +0900

    Feat : S3 파일 업로드 설정

---
+++ src/main/java/com/chicchoc/sivillage/global/infra/presentation/S3Controller.java
@@ -0,0 +1,37 @@
+package com.chicchoc.sivillage.global.infra.presentation;
+
+import com.chicchoc.sivillage.global.common.entity.BaseResponse;
+import com.chicchoc.sivillage.global.infra.application.S3Service;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/s3")
+public class S3Controller {
+    private final S3Service s3Service;
+
+    @PostMapping
+    public BaseResponse<String> uploadFile(@RequestPart("file") MultipartFile file) throws IOException {
+
+        String imageUrl = s3Service.uploadFile(file, "review");
+        return new BaseResponse<>(imageUrl);
+    }
+
+    @GetMapping
+    public BaseResponse<Void> getFile(String category, String fileName) throws IOException {
+        // 테스트용
+        category = "review";
+        // 테스트용
+        fileName = "FLcGUnDoUDm_PHulV_a1b.jpg";
+
+        // String imageUrl = s3Service.getFile(category, fileName);
+        return new BaseResponse<>();
+    }
+}
