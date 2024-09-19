84d9ef86a3f0ed3e5c033030d34c1dab96ced04b
SeongGwang <seonggwangdev@gmail.com>
2024-09-19 15:10:10 +0900

    Refactor : 약관 조회시 약관 종류 추가

--- src/main/java/com/chicchoc/sivillage/domain/terms/presentation/TermsController.java
+++ src/main/java/com/chicchoc/sivillage/domain/terms/presentation/TermsController.java
@@ -1,12 +1,14 @@
 package com.chicchoc.sivillage.domain.terms.presentation;
 
 import com.chicchoc.sivillage.domain.terms.application.TermsService;
+import com.chicchoc.sivillage.domain.terms.domain.TermsType;
 import com.chicchoc.sivillage.domain.terms.dto.in.CreateTermsRequestDto;
 import com.chicchoc.sivillage.domain.terms.dto.out.TermsResponseDto;
 import com.chicchoc.sivillage.global.common.entity.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,10 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TermsController {
 
     private final TermsService termsService;
-    //todo 약관 추가 api
 
-
-    @Operation(summary = "약관 추가", description = "약관 추가(데이터 생성용)")
+    @Operation(summary = "약관 추가(관리자 ONLY)", description = "관리자용 약관데이터 생성")
     @PostMapping("/create")
     public BaseResponse<Void> createTerms(@RequestBody CreateTermsRequestDto createTermsRequestDto) {
 
@@ -33,12 +33,11 @@ public class TermsController {
         return new BaseResponse<>();
     }
 
-    //todo 약관 조회 api(view 상태가 1인 리스트 조회)
-    @Operation(summary = "약관 조회", description = "약관 전체 조회")
+    @Operation(summary = "약관 조회", description = "@Return Map<TermsType, List<Terms>")
     @GetMapping()
-    public BaseResponse<List<TermsResponseDto>> getTerms() {
+    public BaseResponse<Map<TermsType, List<TermsResponseDto>>> getTerms() {
 
-        List<TermsResponseDto> terms = termsService.getTermsHierarchy();
+        Map<TermsType, List<TermsResponseDto>> terms = termsService.getTermsHierarchy();
 
         return new BaseResponse<>(terms);
     }
