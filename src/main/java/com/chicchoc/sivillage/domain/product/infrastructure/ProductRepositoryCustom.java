e5ec080ecde55dd7719481db3eb5d7c6c1e66999
jaehunLee-git <joz526@naver.com>
2024-09-19 14:29:19 +0900

    Feat : ProductRepositoryCustom 및 구현체 추가

---
+++ src/main/java/com/chicchoc/sivillage/domain/product/infrastructure/ProductRepositoryCustom.java
@@ -0,0 +1,16 @@
+package com.chicchoc.sivillage.domain.product.infrastructure;
+
+import com.chicchoc.sivillage.domain.product.domain.Product;
+import com.chicchoc.sivillage.domain.product.dto.in.ProductRequestDto;
+import com.querydsl.core.types.OrderSpecifier;
+
+import java.util.List;
+
+public interface ProductRepositoryCustom {
+
+    // 필터링된 제품 목록을 가져오는 메서드
+    List<Product> findFilteredProducts(ProductRequestDto dto,
+                                       int offset,
+                                       int perPage,
+                                       OrderSpecifier<?> orderSpecifier);
+}
