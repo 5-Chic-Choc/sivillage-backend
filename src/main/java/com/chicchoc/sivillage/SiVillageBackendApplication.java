6ad5f85a8ee0bf5a2dc18b0af39e9112c9e39506
SeongGwang <seonggwangdev@gmail.com>
2024-09-22 14:22:51 +0900

    !HOTFIX : CORS - set @OpenAPIDefinition

--- src/main/java/com/chicchoc/sivillage/SiVillageBackendApplication.java
+++ src/main/java/com/chicchoc/sivillage/SiVillageBackendApplication.java
@@ -1,9 +1,15 @@
 package com.chicchoc.sivillage;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://sivillage.shop", description = "운영 서버")
+        })
 @EnableJpaAuditing
 @SpringBootApplication
 public class SiVillageBackendApplication {
