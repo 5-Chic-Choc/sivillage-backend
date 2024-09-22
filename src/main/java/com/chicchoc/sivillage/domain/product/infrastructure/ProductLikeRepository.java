4bbf8a55a55157f3954d688cb7588231965064bb
jaehunLee-git <joz526@naver.com>
2024-09-22 21:05:01 +0900

    Feat : 좋아요 데이터 처리 로직 수정 및 성능 개선

--- src/main/java/com/chicchoc/sivillage/domain/product/infrastructure/ProductLikeRepository.java
+++ src/main/java/com/chicchoc/sivillage/domain/product/infrastructure/ProductLikeRepository.java
@@ -1,15 +1,14 @@
 package com.chicchoc.sivillage.domain.product.infrastructure;
 
 import com.chicchoc.sivillage.domain.product.domain.ProductLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> {
 
     Optional<ProductLike> findTopByProductUuidAndUserUuidAndIsLiked(String productUuid, String userUuid, Boolean liked);
 
     List<ProductLike> findByUserUuidAndIsLiked(String userUuid, Boolean liked);
-
-
 }
