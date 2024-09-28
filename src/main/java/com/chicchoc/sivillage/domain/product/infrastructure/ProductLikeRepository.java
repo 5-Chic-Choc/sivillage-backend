2e50367c5f8ffef4acd68fafa0bd943d9fbbcfcb
SeongGwang <seonggwangdev@gmail.com>
2024-09-28 23:15:23 +0900

    Fix : 좋아요 로직 수정
    - 내역을 저장하지 않음

--- src/main/java/com/chicchoc/sivillage/domain/product/infrastructure/ProductLikeRepository.java
+++ src/main/java/com/chicchoc/sivillage/domain/product/infrastructure/ProductLikeRepository.java
@@ -10,5 +10,7 @@ public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> 
 
     Optional<ProductLike> findTopByProductUuidAndUserUuidAndIsLiked(String productUuid, String userUuid, Boolean liked);
 
+    Optional<ProductLike> findTopByProductUuidAndUserUuid(String productUuid, String userUuid);
+
     List<ProductLike> findByUserUuidAndIsLiked(String userUuid, Boolean liked);
 }
--- src/main/java/com/chicchoc/sivillage/domain/product/presentation/ProductLikeController.java
+++ src/main/java/com/chicchoc/sivillage/domain/product/presentation/ProductLikeController.java
@@ -24,7 +24,7 @@ public class ProductLikeController {
     @PostMapping("/{productUuid}")
     public BaseResponse<Void> likeProduct(@PathVariable String productUuid, Authentication authentication) {
 
-        productLikeService.saveAndUpdateProductLike(productUuid, authentication.getName());
+        productLikeService.toggleProductLike(productUuid, authentication.getName());
 
         return new BaseResponse<>();
     }
